8c2108453d841ee48d645b4d79536a730e07eef6
Diego <dcalvolozano@gmail.com>
2024-05-23 13:10:58 -0500

    TERMINADO (por ahora)
    boton ir arriba hecho
    retoques al css

--- index.js
+++ index.js
@@ -56,3 +56,19 @@ window.onclick = (event) => {
 };
 
 getComidas();
+
+
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 100){
+    document.querySelector('.go-top-container').classList.add('show');
+  }else{
+    document.querySelector('.go-top-container').classList.remove('show');
+  }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});

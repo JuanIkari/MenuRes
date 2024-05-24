dc72b91879ff3f5ce30a96079b1ebd1a613a15e6
JuanIkari <j.arcila1@utp.edu.co>
2024-05-23 20:21:59 -0500

    PROYECTO FINALIZADO REAL!

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ window.onclick = (event) => {
 getComidas();
 
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > 100) {
+  if (document.documentElement.scrollTop >= 100) {
     document.querySelector(".go-top-container").classList.add("show");
   } else {
     document.querySelector(".go-top-container").classList.remove("show");
